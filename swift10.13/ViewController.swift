b59de87025445ea3bd7c653c9e29bb36b9fd9b4f
807119680 <807119680@qq.com>
2018-10-13 10:16:33 +0800

    一个计算问题
    图形界面

--- swift10.13/ViewController.swift
+++ swift10.13/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var y: UITextField!
+    @IBOutlet weak var z: UITextField!
+    
+    @IBAction func calculate(_ sender: Any) {
+        var a = 0
+        var b = 0
+        a = Int(x.text!)!
+        b = Int(y.text!)!
+        var c = a + b
+        z.text = "\(c)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        x.text = "0"
+        y.text = "0"
+        z.text = "0"
     }
 
 
 }
-
